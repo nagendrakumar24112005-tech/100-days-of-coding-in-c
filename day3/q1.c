e47cfb7088e5a91959a9417b4d84da65fa8d4ec3
nagendrakumar24112005-tech <nagendrakumar24112005@gmail.com>
2025-11-04 09:54:46 +0530

    Add files via upload

---
+++ day3/q1.c
@@ -0,0 +1,10 @@
+//Write a program to convert temperature from Celsius to Fahrenheit.
+#include <stdio.h>
+void main (){
+    
+    int  celcius , fahrenheit ;
+    celcius =100;
+
+    fahrenheit =(celcius*9/5)+32;
+    printf("conversion of 100 degrees into fahrenheit is %d \n", fahrenheit );
+}
